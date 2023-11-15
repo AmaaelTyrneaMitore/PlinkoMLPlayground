9398ca8a520316f4598d5b17fe64f4e8fb9ebf54
Shashank Singh <96770927+AmaaelTyrneaMitore@users.noreply.github.com>
2023-11-15 05:35:00 +0530

    🌟 Update: Implement KNN to work with 1 feature (dropPosition)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
 /**
+ * Stores each observation in this application
+ * Observations include drop position, bounciness, size and bucket label
+ */
+const observations = [];
+// Prediction point
+const predictionPoint = 300;
+// Number of k-Nearest records
+const k = 3;
+
+/**
  * Callback when a ball enters the bucket.
  * Updates observations with new ball drop data.
- * @param dropPosition {number} Position from where the ball dropped
- * @param bounciness {number} Bounciness factor of the ball
- * @param size {number} Size of the ball
- * @param bucketLabel {number} Bucket in which the ball fell into
  */
 const onScoreUpdate = (dropPosition, bounciness, size, bucketLabel) => {
-  console.log(dropPosition, bounciness, size, bucketLabel);
+  observations.push([dropPosition, bounciness, size, bucketLabel]);
 };
 
 /**
- * Analyze function that will run whenever the "Run Analysis!" button is clicked
+ * Analyze function to predict the bucket using k-Nearest Neighbours
  */
 const runAnalysis = () => {
-  console.log('Hello');
+  const predictedBucket = _.chain(observations)
+    // Calculate the distance and associate it with the bucket label
+    .map((observation) => [calculateDistance(observation[0], predictionPoint), observation[3]])
+    // Sort by distance and take the top k records
+    .sortBy((observation) => observation[0])
+    .slice(0, k)
+    // Count the occurrences of each bucket label in the top k records
+    .countBy((observation) => observation[1])
+    // Convert to pairs, sort by occurrences, and get the most common bucket label
+    .toPairs()
+    .sortBy((observation) => observation[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+
+  console.log(`Based on our analysis, your ball is likely to land in Bucket #${predictedBucket}`);
 };
+
+/**
+ * Helper function to calculate the distance from the drop point to the prediction point
+ */
+const calculateDistance = (dropPosition) => Math.abs(dropPosition - predictionPoint);
