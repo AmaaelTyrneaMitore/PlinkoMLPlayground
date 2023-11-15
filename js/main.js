082d87df3009d08195fcdb2c86b78fdbb5644d7f
Shashank Singh <96770927+AmaaelTyrneaMitore@users.noreply.github.com>
2023-11-15 05:49:40 +0530

    🔧 Refactor: Generalize the implementation of KNN

--- js/main.js
+++ js/main.js
@@ -20,23 +20,31 @@ const onScoreUpdate = (dropPosition, bounciness, size, bucketLabel) => {
  * Analyze function to predict the bucket using k-Nearest Neighbours
  */
 const runAnalysis = () => {
-  const predictedBucket = _.chain(observations)
-    // Calculate the distance and associate it with the bucket label
-    .map((observation) => [calculateDistance(observation[0], predictionPoint), observation[3]])
-    // Sort by distance and take the top k records
-    .sortBy((observation) => observation[0])
-    .slice(0, k)
-    // Count the occurrences of each bucket label in the top k records
-    .countBy((observation) => observation[1])
-    // Convert to pairs, sort by occurrences, and get the most common bucket label
-    .toPairs()
-    .sortBy((observation) => observation[1])
-    .last()
-    .first()
-    .parseInt()
-    .value();
-
+  const predictedBucket = knn(observations);
   console.log(`Based on our analysis, your ball is likely to land in Bucket #${predictedBucket}`);
+};
+
+/**
+ * Fully generalized k-Nearest Neighbours function
+ */
+const knn = (data) => {
+  return (
+    _.chain(data)
+      // Calculate the distance and associate it with the bucket label
+      .map((observation) => [calculateDistance(observation[0], predictionPoint), observation[3]])
+      // Sort by distance and take the top k records
+      .sortBy((observation) => observation[0])
+      .slice(0, k)
+      // Count the occurrences of each bucket label in the top k records
+      .countBy((observation) => observation[1])
+      // Convert to pairs, sort by occurrences, and get the most common bucket label
+      .toPairs()
+      .sortBy((observation) => observation[1])
+      .last()
+      .first()
+      .parseInt()
+      .value()
+  );
 };
 
 /**
